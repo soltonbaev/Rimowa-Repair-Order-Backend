d509af39e06d2637cff22ffd19badf44f2bcedd7
DenisUAL <denisandreev64@gmail.com>
2019-08-30 21:41:44 +0300

    testing deploy 5

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-import s3 from "./s3.config";
+import s3 from './s3.config';
 
 export default {
   port: process.env.API_PORT,
@@ -7,18 +7,18 @@ export default {
   fakeS3Host: s3.fakeS3Host,
   fakeS3Port: s3.fakeS3Port,
   postgres: {
-    use_env_variable: process.env.DATABASE_URL,
-    // username: process.env.SQL_USER,
-    // password: process.env.SQL_PASS || "",
-    // host: process.env.SQL_HOST,
-    // port: process.env.SQL_PORT,
-    dialect: "postgres"
-    // logging: false,
-    // pool: {
-    //   max: 50,
-    //   min: 0,
-    //   acquire: 30000,
-    //   idle: 10000
-    // }
-  }
+    database: process.env.TEST_MODE ? 'test' : process.env.SQL_DB,
+    username: process.env.SQL_USER,
+    password: process.env.SQL_PASS || '',
+    host: process.env.SQL_HOST,
+    port: process.env.SQL_PORT,
+    dialect: 'postgres',
+    logging: false,
+    pool: {
+      max: 50,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  },
 };
