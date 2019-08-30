6dc845e3392b16b4bcff590e3ccfaa1cb0388175
DenisUAL <denisandreev64@gmail.com>
2019-08-30 21:05:30 +0300

    testing deploy 2

--- config/index.js
+++ config/index.js
@@ -7,12 +7,12 @@ export default {
   fakeS3Host: s3.fakeS3Host,
   fakeS3Port: s3.fakeS3Port,
   postgres: {
-    database: process.env.DATABASE_URL
+    database: process.env.DATABASE_URL,
     // username: process.env.SQL_USER,
     // password: process.env.SQL_PASS || "",
     // host: process.env.SQL_HOST,
     // port: process.env.SQL_PORT,
-    // dialect: "postgres",
+    dialect: "postgres",
     // logging: false,
     // pool: {
     //   max: 50,
